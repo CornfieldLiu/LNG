00596ed34176a500cf5f3c52063e76f6523bb71e
CornfieldLiu <lovelotuspaste@163.com>
2017-09-06 18:28:22 +0800

    the first code

---
+++ Test/hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: maitian
+    > Mail: lovelotuspaste@163.com 
+    > Created Time: Wed 06 Sep 2017 06:14:51 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+main()
+{	char i;
+
+	for(i=1;i<127;i++)
+		printf("%c",i);
+}
