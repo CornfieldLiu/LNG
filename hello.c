7c43f82ef365141869ad1ef4a3ec478641363c32
CornfieldLiu <lovelotuspaste@163.com>
2017-11-08 17:01:47 +0800

    my first code

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: maitian
+    > Mail: lovelotuspaste@163.com 
+    > Created Time: Wed 08 Nov 2017 04:58:41 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+int main()
+{
+	printf("Hello,world!");
+	return 0;
+}
